452ae63326efaf2e5eee965356a41d17e3e12edf
YN1753 <15347290648@163.com>
2026-06-08 20:00:55 +0800

    Initial Commit

---
+++ DeviceDaily/DeviceDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceDailyApp.swift
+//  DeviceDaily
+//
+//  Created by 迟暮 on 2026/6/8.
+//
+
+import SwiftUI
+
+@main
+struct DeviceDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
